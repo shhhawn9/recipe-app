19e49ba4d74a1432b2cdff7dabb8c432f333fdee
Shawn Gu <shawn.gu.92@gmail.com>
2022-07-18 21:54:35 -0700

    adjust script

--- client/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Button.css';
-export default function Button(props) {
-	return (
-		<button type="button" className={props.type} onClick={() => props.onClick()}>
-			{props.name}
-		</button>
-	);
-}
--- client/src/components/Label.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Label extends React.Component {
-	render() {
-		return <h2>{this.props.name}</h2>;
-	}
-}
