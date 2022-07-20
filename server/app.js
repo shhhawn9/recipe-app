c8b52e6a7ef3e97e1515baf3acbfc526409977c7
Shawn Gu <shawn.gu.92@gmail.com>
2022-07-19 21:04:13 -0700

    update port

--- server/app.js
+++ server/app.js
@@ -15,8 +15,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname + '/public')));
-
+app.use(express.static(path.join(__dirname + '/../client/build')));
+console.log(__dirname);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/recipes', recipesRouter);
